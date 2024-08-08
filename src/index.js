25160b1037405def795d3b73655299689df3c7e1
Prof. Renato Gobet Uzun <renato.uzun@etec.sp.gov.br>
2024-08-07 22:55:43 -0300

    API Version with multiple paths

--- src/index.js
+++ src/index.js
@@ -8,8 +8,43 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import users from "./users.json"
+
+const paths = [
+	{ path: "/user", func: requestUsers },
+	{ path: "/products", func: requestProducts },
+	{ path: "/voice", func: requestVoices },
+];
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('First Cloudflare worker App - Hello World!');
+		let requestPath = new URL(request.url).pathname;
+		let response = new Response("Path not Available");
+
+		console.log(requestPath);
+
+		await paths.forEach( async resourcePath => {
+			if (requestPath.startsWith(resourcePath.path)) {
+				console.log(resourcePath);
+				response = await resourcePath.func(request, env, ctx);
+			}
+		});
+
+
+		console.log("Response: ", response);
+		return response;
 	},
 };
+
+async function requestUsers(request, env, ctx) {
+	console.log("chegou aqui");
+	return Response.json(users);
+}
+
+async function requestProducts(request, env, ctx) {
+	return Response.json({});
+}
+
+async function requestVoices(request, env, ctx) {
+	return new Response("Voices not available");
+}
